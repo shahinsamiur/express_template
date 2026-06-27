4dfa329fde727d4fda50e7bedde7fe7575952a49
shahinsamiur <shahinsamiur@yahoo.com>
2026-06-27 09:30:10 +0600

    feat: prisma connection added

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
-// import { testDBConnection } from "./config/supabase_db";
-// import connectDB from "./config/mongodb_db";
+import {testDBConnection} from "./config/prisma_db";
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     // await testDBConnection();
-    // await connectDB();
+    await testDBConnection();
     app.listen(PORT, () => {
       console.log(` Server running on port ${PORT}`);
     });
